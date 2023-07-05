28513841fc07bd36952ae2314194bdcb08a0e49f
nilegevo <n19ao99ya.s@gmail.com>
2023-07-05 23:44:37 +0900

    test

--- Assets/Scenes/RandomCube.cs
+++ Assets/Scenes/RandomCube.cs
@@ -12,7 +12,7 @@ public class RandomCube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hey
     }
 
     // Update is called once per frame
--- Assets/Scenes/planeContoroller.cs
+++ Assets/Scenes/planeContoroller.cs
@@ -8,7 +8,7 @@ public class planeContoroller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hello
     }
 
     // Update is called once per frame
